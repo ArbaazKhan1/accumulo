d851c9b1a5ff225f8fce4a87317c0388cf389b4a
arbaazkhan1 <bazzy_94@yahoo.com>
2026-09-10 13:53:53 -0400

    undo of toString changes

--- core/src/main/java/org/apache/accumulo/core/metadata/StoredTabletFile.java
+++ core/src/main/java/org/apache/accumulo/core/metadata/StoredTabletFile.java
@@ -150,7 +150,7 @@ public class StoredTabletFile extends AbstractTabletFile<StoredTabletFile> {
 
   @Override
   public String toString() {
-    return toMinimalString();
+    return metadataEntry;
   }
 
   public static StoredTabletFile of(final Text metadataEntry) {
--- test/src/main/java/org/apache/accumulo/test/util/FileMetadataUtil.java
+++ test/src/main/java/org/apache/accumulo/test/util/FileMetadataUtil.java
@@ -68,8 +68,9 @@ public class FileMetadataUtil {
           StoredTabletFile file = fileEntry.getKey();
           DataFileValue dfv = fileEntry.getValue();
           files.put(file, dfv);
-          log.debug("Extent: {}; File: {}; Entries: {}; Size: {}", tabletMetadata.getExtent(), file,
-              dfv.getNumEntries(), dfv.getSize());
+          log.debug("Extent: " + tabletMetadata.getExtent() + "; File Name: " + file.getFileName()
+              + "; Range: " + file.getRange() + "; Entries: " + dfv.getNumEntries() + ", Size: "
+              + dfv.getSize());
           i++;
         }
       }
